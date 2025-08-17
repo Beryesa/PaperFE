a48e6ca8a017ad2971115a632f5e0d99f08aa6ac
Berk Elyesa Yıldırım <beryesa@sayafe.org>
2025-08-17 02:47:01 +0200

    Vendor assets

---
+++ assets/js/license.js
@@ -0,0 +1,8 @@
+/*
+  PaperFE v0.1+
+  License: MIT https://github.com/beryesa/PaperFE/blob/main/LICENSE
+  Copyright (c) 2025 beryesa
+
+  Copyright (c) 2020 nanxiaobei and adityatelange
+  Copyright (c) 2021-2025 adityatelange
+*/
